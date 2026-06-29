10d657610b5f98bfa14fbad777a1a77e48d31329
knightlsy <1292671633@qq.com>
2026-06-29 22:14:07 +0800

    v1.3.6: 修复启动检查更新和CDN下载

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.knightlsy.douyin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.3.5"
+        versionCode = 11
+        versionName = "1.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
