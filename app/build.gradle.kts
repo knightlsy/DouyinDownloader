fc44fe3585c465b6eb6a17b76c83f7002cd989bf
knightlsy <1292671633@qq.com>
2026-09-22 18:07:33 +0000

    v1.3.9: 版本号与更新日志

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.knightlsy.douyin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.3.8"
+        versionCode = 14
+        versionName = "1.3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
